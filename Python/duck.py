f164021105106d34823654c4e0ea43421b1377d1
Konstantin Kuzmin <kmkuzmin@gmail.com>
2024-02-07 16:42:45 -0500

    Updated duck.py to print all properties and methods of the objects in question.

--- Python/duck.py
+++ Python/duck.py
@@ -17,6 +17,9 @@ duck = Duck()
 airplane = Airplane()
 whale = Whale()
 
+print(dir(duck))
 lift_off(duck) # prints `Duck flying`
+print(dir(airplane))
 lift_off(airplane) # prints `Airplane flying`
+print(dir(whale))
 lift_off(whale) # Throws the error `'Whale' object has no attribute 'fly'`
